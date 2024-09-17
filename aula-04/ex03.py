bef79d9ea374e6ab7fc84fd3bed9ee5c4a608372
Stiverson <stiverson.machado@gmail.com>
2024-09-17 19:56:39 -0300

    Correção exercício 3 da lista da aula 4

--- aula-04/ex03.py
+++ aula-04/ex03.py
@@ -1,9 +1,9 @@
 primeiroValor = int(input('Digite o primeiro  valor: '))
 segundoValor = int(input('Digite o segundo valor: '))
 
-divisao = primeiroValor / segundoValor
 
 if segundoValor == 0:
     print('O valor inserido não pode ser zero')
 else:
+    divisao = primeiroValor / segundoValor
     print('A soma dos dois valores é : ',divisao)
