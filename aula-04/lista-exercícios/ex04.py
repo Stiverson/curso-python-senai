6a8e7450e2a42c71182913828b5fba9af5263675
Stiverson <stiverson.machado@gmail.com>
2024-09-17 20:56:47 -0300

    Organização dos exercícios atuais do curso

--- aula-04/lista-exercícios/ex04.py
+++ aula-04/lista-exercícios/ex04.py
@@ -4,8 +4,8 @@ segundaNota = float(input('Digite a segunda nota: '))
 pesoPrimeiraNota = 2
 pesoSegundaNota = 3
 
-mediaPrimeiranota = primeiroNota / pesoPrimeiraNota
-mediaSegundaNota = segundaNota / pesoSegundaNota
+mediaPrimeiranota = primeiroNota * pesoPrimeiraNota
+mediaSegundaNota = segundaNota * pesoSegundaNota
 
 print('A média da primeira avaliação é: ', mediaPrimeiranota)
 print('A média da segunda avaliação é : ', mediaSegundaNota)
---
+++ aula-04/lista-exercícios/ex07.py
@@ -0,0 +1,8 @@
+pesoPessoa = float(input('Digite o seu peso: '))
+
+pesoEngordar = pesoPessoa * 0.15 + pesoPessoa
+pesoEmagrecer = pesoPessoa * 0.20 + pesoPessoa
+
+
+print('Seu peso caso você engorde 15% é : ',pesoEngordar)
+print('Seu peso caso você engorde 15% é : ', pesoEmagrecer)
