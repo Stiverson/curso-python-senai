4fef426dedb335b39b66c6434e2a9c360b90e50d
Stiverson <stiverson.machado@gmail.com>
2024-09-19 21:54:11 -0300

    Correção e ajustes aula 06

--- aula-06/lista-exercicios-aula-06/ex03.py
+++ aula-06/lista-exercicios-aula-06/ex03.py
@@ -1,15 +1,21 @@
-a = float(input("Primeiro número:"))
-b = float(input("Segundo número:"))
-operação = input("Digite a operação a realizar (+,-,* ou /):")
-if operação == "+":
-    resultado = a + b
-elif operação == "-":
-    resultado = a - b
-elif operação == "*":
-    resultado = a * b
-elif operação == "/":
-    resultado = a / b
-else:
-    print("Operação inválida!")
-    resultado = 0
-print("Resultado: ", resultado)
+while True:
+    primeiroNumero = float(input("Primeiro número: "))
+    segundoNumero = float(input("Segundo número: "))
+    operação = input("Digite a operação a realizar (+, -, * ou /) ou 'sair' para encerrar: ")
+
+    if operação == "sair":
+        print("Encerrando o programa.")
+        break
+    elif operação == "+":
+        resultado = primeiroNumero + segundoNumero
+    elif operação == "-":
+        resultado = primeiroNumero - segundoNumero
+    elif operação == "*":
+        resultado = primeiroNumero * segundoNumero
+    elif operação == "/":
+        resultado = primeiroNumero / segundoNumero
+    else:
+        print("Operação inválida!")
+        resultado = 0
+
+    print("Resultado: ", resultado)
