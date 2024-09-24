f624160384803c870efe32b3c5bd8a5e2ef93827
Stiverson <stiverson.machado@gmail.com>
2024-09-24 20:52:16 -0300

    ex01 aula 07 adicionado

---
+++ aula-07/lista-de-exercicios/ex01.py
@@ -0,0 +1,10 @@
+def verificar(num_1,num_2):
+    if num_1 >= num_2:
+        print("O primeiro numero digitado {} é maior:".format(num_1))
+    else:
+        print("O Segundo  digitado {} é maior:".format(num_2))
+        
+num_1 = int(input("Digite um numero : "))
+num_2 = int(input("Digite outro numero : "))
+
+verificar(num_1,num_2)
