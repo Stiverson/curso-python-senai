4d34b4535fc8db46c475b34a37e761c4fc19bdea
Stiverson Machado <stiverson.machado@gmail.com>
2024-09-25 16:00:25 -0300

    Adição do exercícios de validação de cpf

---
+++ Validação-cpf/validador.py
@@ -0,0 +1,32 @@
+def valida_cpf(cpf):
+    cpf = ''.join(filter(str.isdigit, cpf))
+
+    
+    if len(cpf) != 11:
+        return False
+
+   
+    soma = 0
+    for i in range(9):
+        soma += int(cpf[i]) * (10 - i)
+    resto = soma % 11
+    digito1 = 0 if resto < 2 else 11 - resto
+
+    
+    soma = 0
+    for i in range(10):
+        soma += int(cpf[i]) * (11 - i)
+    resto = soma % 11
+    digito2 = 0 if resto < 2 else 11 - resto
+
+   
+    if digito1 == int(cpf[9]) and digito2 == int(cpf[10]):
+        return True
+    else:
+        return False
+
+cpf = input("Digite o CPF: ")
+if valida_cpf(cpf):
+    print("CPF válido")
+else:
+    print("CPF inválido")
