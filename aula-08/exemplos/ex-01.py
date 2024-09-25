c1244dc7bd1607bc3436f3701ab090800b55c5d2
Stiverson <stiverson.machado@gmail.com>
2024-09-25 19:13:46 -0300

    Adição aula 08

---
+++ aula-08/exemplos/ex-01.py
@@ -0,0 +1,8 @@
+predio   =  ["Térreo","Primeiro andar","Segundo andar","Terceiro andar","Quarto andar","Quinto andar"]
+
+print(predio[0])
+print(predio[1])
+print(predio[2])
+print(predio[3])
+print(predio[4])
+print(predio[5])
