eb965fdb5a15bbd7285ee3573601b81882ca12e8
Stiverson <stiverson.machado@gmail.com>
2024-09-25 21:25:09 -0300

    Correção ex 03 da lista de exercicos da aula 08

--- aula-08/lista-exercícios/ex-03.py
+++ aula-08/lista-exercícios/ex-03.py
@@ -1,2 +1,19 @@
 # Faça um programa que leia 4 notas, armazene em um vetor e mostre as
 # notas e a média na tela.
+
+notas = [0,0,0,0]
+
+soma = 0
+x =0
+
+while x < 4:
+    notas[x] = float(input("Nota %d: " % x ))
+    soma += notas[x]
+    x += 1
+x = 0
+while x < 4:
+    print("Nota %d: %6.2f" % (x, notas[x]))
+    x += 1
+
+media   = soma / x
+print("Media: %5.2f" % media)
